f01799a4db8a024e80b05d4e94b7168a9372a4f6
lout33 <sssimple.33@gmail.com>
2022-06-18 22:37:57 -0500

    add m79

--- index.js
+++ index.js
@@ -135,7 +135,7 @@ export default e => {
     }
     
     {
-      let u2 = `${baseUrl}military.glb`;
+      let u2 = `${baseUrl}m79.glb`;
       if (/^https?:/.test(u2)) {
         u2 = '/@proxy/' + u2;
       }
